4ac973ed453fc3c21b8487c6b6651d6128872b82
Brian Capouch <brianc@palaver.net>
2016-03-26 20:00:43 -0400

    Initial commit

---
+++ client/layouts/mainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const MainLayout = ({navbar, content, footer}) => (
+  <div>
+      {navbar}
+      <div className="container">
+        {content}
+      </div>
+      {footer}
+  </div>
+);
+
